b8629ee2361a6e707fb2ec56d3ea4ba495344f03
Thomas Ryan <tomryan_8_1997@hotmail.com>
2024-11-11 20:20:12 -0500

    Removed unused script

--- Jetman/ray_cast_2d.gd
+++
@@ -1,9 +0,0 @@
-extends RayCast2D
-
-@export var state_machine: StateMachine
-@export var slide_state: SlideState
-
-func _physics_process(delta: float) -> void:
-	if is_colliding():
-		if state_machine.current_state is not SlideState:
-			state_machine.change_state(slide_state)
